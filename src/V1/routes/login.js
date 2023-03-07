485f021a9ac517a894941bc768e56d102c323dd5
NGCnathalia <nthaliagarcia@gmail.com>
2023-03-07 12:30:45 -0500

    Change in route

--- src/V1/routes/login.js
+++ src/V1/routes/login.js
@@ -4,10 +4,10 @@ const {validateCreate}=require('../../validators/logins');
 const {validateCreateUser}=require('../../validators/users');
 const { loginCtrl, registerCtrl } = require('../../controller/loginController')
 
-// localhost:3000/Api/v1/ligin/
+// localhost:3000/Api/v1/login/
 router.post('/', validateCreate,loginCtrl)
 
-// localhost:3000/Api/v1/ligin/register/
+// localhost:3000/Api/v1/login/register/
 router.post('/register',validateCreateUser,  registerCtrl)
 
 
