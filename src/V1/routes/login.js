e60fce57b9f5144d9c735b106c95b41fdb0701e4
ruthyi <jbruthlizcano@gmail.com>
2023-03-06 21:57:14 -0500

    merge featire1 and feature2 with develop

--- src/V1/routes/login.js
+++ src/V1/routes/login.js
@@ -3,10 +3,10 @@ const router = express.Router()
 //const {validateCreate}=require('../../validators/logins');
 const { loginCtrl, registerCtrl } = require('../../controller/loginController')
 
-
+// localhost:3000/Api/v1/ligin/
 router.post('/', loginCtrl)
 
-
+// localhost:3000/Api/v1/ligin/register/
 router.post('/register', registerCtrl)
 
 
--- src/V1/routes/user.js
+++ src/V1/routes/user.js
@@ -4,18 +4,18 @@ const router = express.Router()
 //const checkRoleAuth = require('../middleware/roleAuth')
 const {getUsers, getUserById, createUser, updateUser, updateUserState} = require('../../controller/userController');
 
-  
+//localhot:3000/Api/v1/user  
 router.get('/', getUsers)
 
 
-
+//localhot:3000/Api/v1/user/id  
 router.get('/:id', getUserById)
 
 
-
+//localhot:3000/Api/v1/user
 router.post('/',  createUser)
 
-
+//localhot:3000/Api/v1/user/id
 router.put('/:id',  updateUser)
 
 
