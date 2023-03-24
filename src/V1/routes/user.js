d59207796bf1b633ac23881d26c25c3b0008abc8
ruthyi <jbruthlizcano@gmail.com>
2023-03-24 11:04:09 -0500

    add route in user

--- src/V1/routes/user.js
+++ src/V1/routes/user.js
@@ -8,12 +8,74 @@ const {validateCreatePutUser}=require('../../validators/putUser');
 const {getUsers, getUserById, createUser, updateUser, updateUserState, getUserByName} = require('../../controller/userController');
 
 //localhot:3000/Api/v1/user  
+ /**
+ * @swagger
+ * /Api/v1/user/:
+ *   get:
+ *     summary: Listado de usuarios Registrados
+ *     description: Este endpoint unicamente lo podra utilizar un usuario con rol super admin
+ *     tags:
+ *       - Users
+ *     responses:
+ *       '200':
+ *         description: Acción  exitosa
+ *       '409':
+ *         description: Error en la petición
+ *     security:
+ *       - bearerAuth: []
+ */
 router.get('/', checkAuth, checkRoleAuth(['superAdmin']),getUsers)
 
 
-//localhot:3000/Api/v1/user/id  
+/**
+ * @swagger
+ * /Api/v1/user/{id}:
+ *   get:
+ *     summary: Muestra un usuario de acuerdo al Id
+ *     description: Este endpoint unicamente lo podra utilizar un usuario con rol admin
+ *     tags:
+ *       - Users
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: ID del recurso
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: Respuesta exitosa
+ *       '400':
+ *         description: Error en la petición
+ *     security:
+ *       - bearerAuth: []
+ */
 router.get('/:id',checkAuth, checkRoleAuth(['superAdmin']), getUserById)
-//localhot:3000/Api/v1/user/name  
+
+
+/**
+ * @swagger
+ * /Api/v1/user/name/{name}:
+ *   get:
+ *     summary: Muestra un usuario de acuerdo al nombre
+ *     description: Este endpoint unicamente lo podra utilizar un usuario con rol admin
+ *     tags:
+ *       - Users
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: ID del recurso
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: Respuesta exitosa
+ *       '400':
+ *         description: Error en la petición
+ *     security:
+ *       - bearerAuth: []
+ */
 router.get('/name/:name',checkAuth, checkRoleAuth(['superAdmin']), getUserByName)
 
 //localhot:3000/Api/v1/user
